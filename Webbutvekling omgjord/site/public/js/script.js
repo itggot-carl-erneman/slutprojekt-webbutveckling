948e86032a8a06cfd1dd3f780e319c0f145bdc8b
itggot-carl-erneman <carl.erneman@itggot.se>
2018-05-26 00:26:40 +0200

    Allt är skit. Fick göra om allt

---
+++ Webbutvekling omgjord/site/public/js/script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    var element = document.querySelector(".menu");
+    element.classList.toggle("show");
+
+    var element = document.querySelector(".menu");
+    element.classList.toggle("fade");
+}
